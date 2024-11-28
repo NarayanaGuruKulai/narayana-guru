837e22bd65a483351d05abf6cb6f0395d69b2f47
Shishir Karkera <159174711+shishirkarkeraa@users.noreply.github.com>
2024-11-28 17:12:35 +0530

    Update User.tsx

--- src/components/Tabs/User.tsx
+++ src/components/Tabs/User.tsx
@@ -40,19 +40,19 @@ const User = () => {
       <table className="min-w-full border border-gray-300 bg-black">
         <thead>
           <tr className="bg-gray-200">
-            <th className="text-black border border-gr py-2 px-4 border-b border-slate-700 text-center">Email</th>
-            <th className="text-black border border-gr py-2 px-4 border-b border-slate-700 text-center">Role</th>
-            <th className="text-black border border-gr py-2 px-4 border-b border-slate-700 text-center">Change Role</th>
+            <th className="text-black border border-gr py-2 px-4 border-b border-slate-700 text-center text-sm md:text-xl ">Email</th>
+            <th className="text-black border border-gr py-2 px-4 border-b border-slate-700 text-center text-sm md:text-xl">Role</th>
+            <th className="text-black border border-gr py-2 px-4 border-b border-slate-700 text-center text-sm md:text-xl ">Change Role</th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => (
             <tr key={user.id} className="text-center">
-              <td className="py-2 px-4 border-b border-slate-700 text-center text-wrap">{user.email}</td>
-              <td className="py-2 px-4 border-b border-slate-700 text-center">{user.role}</td>
-              <td className="py-2 px-4 border-b border-slate-700 text-center">
+              <td className="py-2 px-4 border-b border-slate-700 text-center text-wrap text-sm md:text-xl ">{user.email}</td>
+              <td className="py-2 px-4 border-b border-slate-700 text-center text-sm md:text-xl">{user.role}</td>
+              <td className="py-2 px-4 border-b border-slate-700 text-center md:text-xl text-sm">
                 <button 
-                  className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600"
+                  className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600 text-sm md:text-xl"
                   onClick={() => {
                     setSelectedUserId(user.id);
                     setNewRole(user.role);  // Set the default role to the user's current role
